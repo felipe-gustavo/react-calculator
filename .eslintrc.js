1f8c32f6961caae103a954e98057667a8fd79b8b
Felipe Gustavo <felipegustavo1605@gmail.com>
2022-07-16 10:25:17 -0300

    Initialize project using Create React App

---
+++ .eslintrc.js
@@ -0,0 +1,108 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "airbnb",
+    "eslint-config-prettier",
+    "prettier",
+    "plugin:react/recommended",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jest/recommended",
+    "plugin:testing-library/react",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "import", "eslint-plugin-prettier"],
+  rules: {
+    // Prettier code style seetings
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    // React extensions
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".tsx"],
+      },
+    ],
+    // Change console to warn
+    "no-console": "warn",
+    // Ignore extension on import ts and js files
+    "import/extensions": [
+      "error",
+      "never",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+        json: "always",
+      },
+    ],
+    // Import requirements
+    "import/order": [
+      "error",
+      {
+        // Alphabetic import
+        alphabetize: {
+          caseInsensitive: true,
+          order: "asc",
+        },
+        // Group imports
+        groups: [
+          "builtin",
+          "external",
+          "index",
+          "sibling",
+          "parent",
+          "internal",
+        ],
+        // Split imports on new lines
+        "newlines-between": "always-and-inside-groups",
+      },
+    ],
+    // Disable export default
+    "import/prefer-default-export": ["off"],
+    // Enable import from devDep
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: true,
+      },
+    ],
+    // Turn off rules to setup them on typescript
+    "no-useless-constructor": "off",
+    // Error on useless constructor
+    "@typescript-eslint/no-useless-constructor": ["error"],
+  },
+  settings: {
+    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {},
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
